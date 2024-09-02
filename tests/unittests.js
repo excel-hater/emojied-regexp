eac282deaf65a452e531d26b77b1f6fe0268efa6
excel-hater <m3398412525@gmail.com>
2024-09-02 09:19:19 +0900

    Live Previewを使ったテストできそう

--- tests/unittests.js
+++ tests/unittests.js
@@ -1,8 +1,22 @@
-if(
-  isExpected("foo","foo")
-  && isExpected("foo","bar", "wrong")
-){
-  console.debug("all ok");
-}else{
-  console.debug("something goes wrong");
+function getResult(actual, expected, name="no-name"){
+  if (actual === expected){
+    return `PASS: ${name} .\n`;
+  }
+  else{
+    return `FAIL: ${name} expected: ${JSON.stringify(expected, null, '\t')} but actual: ${JSON.stringify(actual, null, '\t')} .\n`;
+  }
+}
+
+function writeSomewhere(str){
+  document.body.innerText += str;
+}
+
+function doTest(actual, expected, name="no-name"){
+  writeSomewhere(getResult(actual, expected, name));
+}
+
+// 
+function doUnitTests(){
+  doTest("foo","foo");
+  doTest("foo","bar", "wrong");
 }
